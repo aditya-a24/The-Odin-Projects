53d5eae6a9351e21c8b5422f5b58e9b39c021c6f
aditya_A <asquare2425@gmail.com>
2024-09-16 22:27:32 +0530

    completed project Etch-a-Sketch

--- JS/02 Etch-a-Sketch/script.js
+++ JS/02 Etch-a-Sketch/script.js
@@ -1,515 +1,62 @@
-const container1 = document.querySelector("#container1");
-const div11 = document.createElement("div");
-const div12 = document.createElement("div");
-const div13 = document.createElement("div");
-const div14 = document.createElement("div");
-const div15 = document.createElement("div");
-const div16 = document.createElement("div");
-const div17 = document.createElement("div");
-const div18 = document.createElement("div");
-const div19 = document.createElement("div");
-const div110 = document.createElement("div");
-const div111 = document.createElement("div");
-const div112 = document.createElement("div");
-const div113 = document.createElement("div");
-const div114 = document.createElement("div");
-const div115 = document.createElement("div");
-const div116 = document.createElement("div");
+const container = document.querySelector("#container");
+let useRandomColor = false; // to toggle between black and rgb
 
-const container2 = document.querySelector("#container2");
-const div21 = document.createElement("div");
-const div22 = document.createElement("div");
-const div23 = document.createElement("div");
-const div24 = document.createElement("div");
-const div25 = document.createElement("div");
-const div26 = document.createElement("div");
-const div27 = document.createElement("div");
-const div28 = document.createElement("div");
-const div29 = document.createElement("div");
-const div210 = document.createElement("div");
-const div211 = document.createElement("div");
-const div212 = document.createElement("div");
-const div213 = document.createElement("div");
-const div214 = document.createElement("div");
-const div215 = document.createElement("div");
-const div216 = document.createElement("div");
+function createGrid(squaresPerSide) {
+
+    //clear the container for the new grid
+    container.innerHTML = '';
+    const squareSize = 960 / squaresPerSide; // it will gove size of each square of grid
+
+    // create grid of divs
+    for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+        const square = document.createElement("div");
+        square.classList.add("grid-square");
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        // Add hover effect to change color on mouse over
+        square.addEventListener('mouseover', () => {
+            if (useRandomColor) {
+                square.style.backgroundColor = getRandomColor();
+            } else {
+                square.style.backgroundColor = 'black';
+            }
+        });
+
+        container.appendChild(square);
+    }
+}
+
+createGrid(16);
 
 
-const container3 = document.querySelector("#container3");
-const div31 = document.createElement("div");
-const div32 = document.createElement("div");
-const div33 = document.createElement("div");
-const div34 = document.createElement("div");
-const div35 = document.createElement("div");
-const div36 = document.createElement("div");
-const div37 = document.createElement("div");
-const div38 = document.createElement("div");
-const div39 = document.createElement("div");
-const div310 = document.createElement("div");
-const div311 = document.createElement("div");
-const div312 = document.createElement("div");
-const div313 = document.createElement("div");
-const div314 = document.createElement("div");
-const div315 = document.createElement("div");
-const div316 = document.createElement("div");
+// taking user input for square on each side of the grid
+function promptGridSize() {
+    let userInput = parseInt(prompt("Enter the number of square per side (max 100): "));
 
-const container4 = document.querySelector("#container4");
-const div41 = document.createElement("div");
-const div42 = document.createElement("div");
-const div43 = document.createElement("div");
-const div44 = document.createElement("div");
-const div45 = document.createElement("div");
-const div46 = document.createElement("div");
-const div47 = document.createElement("div");
-const div48 = document.createElement("div");
-const div49 = document.createElement("div");
-const div410 = document.createElement("div");
-const div411 = document.createElement("div");
-const div412 = document.createElement("div");
-const div413 = document.createElement("div");
-const div414 = document.createElement("div");
-const div415 = document.createElement("div");
-const div416 = document.createElement("div");
+    if (userInput > 0 && userInput <= 100) {
+        createGrid(userInput);
+    } else {
+        alert("Please enter a number between 1 and 100.");
+    }
+    console.log(userInput);
+}
 
-const container5 = document.querySelector("#container5");
-const div51 = document.createElement("div");
-const div52 = document.createElement("div");
-const div53 = document.createElement("div");
-const div54 = document.createElement("div");
-const div55 = document.createElement("div");
-const div56 = document.createElement("div");
-const div57 = document.createElement("div");
-const div58 = document.createElement("div");
-const div59 = document.createElement("div");
-const div510 = document.createElement("div");
-const div511 = document.createElement("div");
-const div512 = document.createElement("div");
-const div513 = document.createElement("div");
-const div514 = document.createElement("div");
-const div515 = document.createElement("div");
-const div516 = document.createElement("div");
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
 
-const container6 = document.querySelector("#container6");
-const div61 = document.createElement("div");
-const div62 = document.createElement("div");
-const div63 = document.createElement("div");
-const div64 = document.createElement("div");
-const div65 = document.createElement("div");
-const div66 = document.createElement("div");
-const div67 = document.createElement("div");
-const div68 = document.createElement("div");
-const div69 = document.createElement("div");
-const div610 = document.createElement("div");
-const div611 = document.createElement("div");
-const div612 = document.createElement("div");
-const div613 = document.createElement("div");
-const div614 = document.createElement("div");
-const div615 = document.createElement("div");
-const div616 = document.createElement("div");
+function toggleRandomColor() {
+    useRandomColor = !useRandomColor;
+    alert(`Random color mode is now ${useRandomColor ? "ON" : "OFF"}.`);
+}
 
-const container7 = document.querySelector("#container7");
-const div71 = document.createElement("div");
-const div72 = document.createElement("div");
-const div73 = document.createElement("div");
-const div74 = document.createElement("div");
-const div75 = document.createElement("div");
-const div76 = document.createElement("div");
-const div77 = document.createElement("div");
-const div78 = document.createElement("div");
-const div79 = document.createElement("div");
-const div710 = document.createElement("div");
-const div711 = document.createElement("div");
-const div712 = document.createElement("div");
-const div713 = document.createElement("div");
-const div714 = document.createElement("div");
-const div715 = document.createElement("div");
-const div716 = document.createElement("div");
-
-const container8 = document.querySelector("#container8");
-const div81 = document.createElement("div");
-const div82 = document.createElement("div");
-const div83 = document.createElement("div");
-const div84 = document.createElement("div");
-const div85 = document.createElement("div");
-const div86 = document.createElement("div");
-const div87 = document.createElement("div");
-const div88 = document.createElement("div");
-const div89 = document.createElement("div");
-const div810 = document.createElement("div");
-const div811 = document.createElement("div");
-const div812 = document.createElement("div");
-const div813 = document.createElement("div");
-const div814 = document.createElement("div");
-const div815 = document.createElement("div");
-const div816 = document.createElement("div");
-
-const container9 = document.querySelector("#container9");
-const div91 = document.createElement("div");
-const div92 = document.createElement("div");
-const div93 = document.createElement("div");
-const div94 = document.createElement("div");
-const div95 = document.createElement("div");
-const div96 = document.createElement("div");
-const div97 = document.createElement("div");
-const div98 = document.createElement("div");
-const div99 = document.createElement("div");
-const div910 = document.createElement("div");
-const div911 = document.createElement("div");
-const div912 = document.createElement("div");
-const div913 = document.createElement("div");
-const div914 = document.createElement("div");
-const div915 = document.createElement("div");
-const div916 = document.createElement("div");
-
-const container10 = document.querySelector("#container10");
-const div101 = document.createElement("div");
-const div102 = document.createElement("div");
-const div103 = document.createElement("div");
-const div104 = document.createElement("div");
-const div105 = document.createElement("div");
-const div106 = document.createElement("div");
-const div107 = document.createElement("div");
-const div108 = document.createElement("div");
-const div109 = document.createElement("div");
-const div1010 = document.createElement("div");
-const div1011 = document.createElement("div");
-const div1012 = document.createElement("div");
-const div1013 = document.createElement("div");
-const div1014 = document.createElement("div");
-const div1015 = document.createElement("div");
-const div1016 = document.createElement("div");
-
-const container11 = document.querySelector("#container11");
-const div1101 = document.createElement("div");
-const div1102 = document.createElement("div");
-const div1103 = document.createElement("div");
-const div1104 = document.createElement("div");
-const div1105 = document.createElement("div");
-const div1106 = document.createElement("div");
-const div1107 = document.createElement("div");
-const div1108 = document.createElement("div");
-const div1109 = document.createElement("div");
-const div1110 = document.createElement("div");
-const div1111 = document.createElement("div");
-const div1112 = document.createElement("div");
-const div1113 = document.createElement("div");
-const div1114 = document.createElement("div");
-const div1115 = document.createElement("div");
-const div1116 = document.createElement("div");
-
-const container12 = document.querySelector("#container12");
-const div121 = document.createElement("div");
-const div122 = document.createElement("div");
-const div123 = document.createElement("div");
-const div124 = document.createElement("div");
-const div125 = document.createElement("div");
-const div126 = document.createElement("div");
-const div127 = document.createElement("div");
-const div128 = document.createElement("div");
-const div129 = document.createElement("div");
-const div1210 = document.createElement("div");
-const div1211 = document.createElement("div");
-const div1212 = document.createElement("div");
-const div1213 = document.createElement("div");
-const div1214 = document.createElement("div");
-const div1215 = document.createElement("div");
-const div1216 = document.createElement("div");
-
-const container13 = document.querySelector("#container13");
-const div131 = document.createElement("div");
-const div132 = document.createElement("div");
-const div133 = document.createElement("div");
-const div134 = document.createElement("div");
-const div135 = document.createElement("div");
-const div136 = document.createElement("div");
-const div137 = document.createElement("div");
-const div138 = document.createElement("div");
-const div139 = document.createElement("div");
-const div1310 = document.createElement("div");
-const div1311 = document.createElement("div");
-const div1312 = document.createElement("div");
-const div1313 = document.createElement("div");
-const div1314 = document.createElement("div");
-const div1315 = document.createElement("div");
-const div1316 = document.createElement("div");
-
-const container14 = document.querySelector("#container14");
-const div141 = document.createElement("div");
-const div142 = document.createElement("div");
-const div143 = document.createElement("div");
-const div144 = document.createElement("div");
-const div145 = document.createElement("div");
-const div146 = document.createElement("div");
-const div147 = document.createElement("div");
-const div148 = document.createElement("div");
-const div149 = document.createElement("div");
-const div1410 = document.createElement("div");
-const div1411 = document.createElement("div");
-const div1412 = document.createElement("div");
-const div1413 = document.createElement("div");
-const div1414 = document.createElement("div");
-const div1415 = document.createElement("div");
-const div1416 = document.createElement("div");
-
-const container15 = document.querySelector("#container15");
-const div151 = document.createElement("div");
-const div152 = document.createElement("div");
-const div153 = document.createElement("div");
-const div154 = document.createElement("div");
-const div155 = document.createElement("div");
-const div156 = document.createElement("div");
-const div157 = document.createElement("div");
-const div158 = document.createElement("div");
-const div159 = document.createElement("div");
-const div1510 = document.createElement("div");
-const div1511 = document.createElement("div");
-const div1512 = document.createElement("div");
-const div1513 = document.createElement("div");
-const div1514 = document.createElement("div");
-const div1515 = document.createElement("div");
-const div1516 = document.createElement("div");
-
-const container16 = document.querySelector("#container16");
-const div161 = document.createElement("div");
-const div162 = document.createElement("div");
-const div163 = document.createElement("div");
-const div164 = document.createElement("div");
-const div165 = document.createElement("div");
-const div166 = document.createElement("div");
-const div167 = document.createElement("div");
-const div168 = document.createElement("div");
-const div169 = document.createElement("div");
-const div1610 = document.createElement("div");
-const div1611 = document.createElement("div");
-const div1612 = document.createElement("div");
-const div1613 = document.createElement("div");
-const div1614 = document.createElement("div");
-const div1615 = document.createElement("div");
-const div1616 = document.createElement("div");
-
-// div1.classList.add("grid");
-// div2.classList.add("grid");
-// div3.classList.add("grid");
-// div4.classList.add("grid");
-// div5.classList.add("grid");
-// div6.classList.add("grid");
-// div7.classList.add("grid");
-// div8.classList.add("grid");
-// div9.classList.add("grid");
-// div10.classList.add("grid");
-// div11.classList.add("grid");
-// div12.classList.add("grid");
-// div13.classList.add("grid");
-// div14.classList.add("grid");
-// div15.classList.add("grid");
-// div16.classList.add("grid");
-
-container1.append(div11, div12, div13, div14, div15, div16, div17, div18, div19, div110, div111, div112, div113, div114, div115, div116);
-
-container2.append(div21,
-    div22,
-    div23,
-    div24,
-    div25,
-    div26,
-    div27,
-    div28,
-    div29,
-    div210,
-    div211,
-    div212,
-    div213,
-    div214,
-    div215,
-    div216);
-
-container3.append(div31, div32, div33, div34, div35, div36, div37, div38, div39, div310, div311, div312, div313, div314, div315, div316);
-
-container4.append(div41, div42, div43, div44, div45, div46, div47, div48, div49, div410, div411, div412, div413, div414, div415, div416);
-
-container5.append(div51, div52, div53, div54, div55, div56, div57, div58, div59, div510, div511, div512, div513, div514, div515, div516);
-
-container6.append(div61, div62, div63, div64, div65, div66, div67, div68, div69, div610, div611, div612, div613, div614, div615, div616);
-
-container7.append(
-    div71,
-    div72,
-    div73,
-    div74,
-    div75,
-    div76,
-    div77,
-    div78,
-    div79,
-    div710,
-    div711,
-    div712,
-    div713,
-    div714,
-    div715,
-    div716);
-
-container8.append(
-    div81,
-    div82,
-    div83,
-    div84,
-    div85,
-    div86,
-    div87,
-    div88,
-    div89,
-    div810,
-    div811,
-    div812,
-    div813,
-    div814,
-    div815,
-    div816);
-
-container9.append(
-    div91,
-    div92,
-    div93,
-    div94,
-    div95,
-    div96,
-    div97,
-    div98,
-    div99,
-    div910,
-    div911,
-    div912,
-    div913,
-    div914,
-    div915,
-    div916);
-
-container10.append(
-    div101,
-    div102,
-    div103,
-    div104,
-    div105,
-    div106,
-    div107,
-    div108,
-    div109,
-    div1010,
-    div1011,
-    div1012,
-    div1013,
-    div1014,
-    div1015,
-    div1016);
-
-
-container11.append(
-    div1101,
-    div1102,
-    div1103,
-    div1104,
-    div1105,
-    div1106,
-    div1107,
-    div1108,
-    div1109,
-    div1110,
-    div1111,
-    div1112,
-    div1113,
-    div1114,
-    div1115,
-    div1116);
-
-container12.append(
-    div121,
-    div122,
-    div123,
-    div124,
-    div125,
-    div126,
-    div127,
-    div128,
-    div129,
-    div1210,
-    div1211,
-    div1212,
-    div1213,
-    div1214,
-    div1215,
-    div1216);
-
-
-container13.append(
-    div131,
-    div132,
-    div133,
-    div134,
-    div135,
-    div136,
-    div137,
-    div138,
-    div139,
-    div1310,
-    div1311,
-    div1312,
-    div1313,
-    div1314,
-    div1315,
-    div1316);
-
-container14.append(
-    div141,
-    div142,
-    div143,
-    div144,
-    div145,
-    div146,
-    div147,
-    div148,
-    div149,
-    div1410,
-    div1411,
-    div1412,
-    div1413,
-    div1414,
-    div1415,
-    div1416);
-
-container15.append(
-    div151,
-    div152,
-    div153,
-    div154,
-    div155,
-    div156,
-    div157,
-    div158,
-    div159,
-    div1510,
-    div1511,
-    div1512,
-    div1513,
-    div1514,
-    div1515,
-    div1516);
-
-container16.append(
-    div161,
-    div162,
-    div163,
-    div164,
-    div165,
-    div166,
-    div167,
-    div168,
-    div169,
-    div1610,
-    div1611,
-    div1612,
-    div1613,
-    div1614,
-    div1615,
-    div1616);
-
+function resetGrid() {
+    const squares = document.querySelectorAll(".grid-square");
+    squares.forEach(square => {
+        square.style.backgroundColor = "white";
+    })
+}
